02319a8c9621a27925160bfe2be8a87c15a52e39
vvyvi <ewawozny4@gmail.com>
2021-03-19 19:50:02 +0100

    init

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 import generateRk from "./functions/generateRk";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [states, setStates] = useState([])
   const [names, setNames] = useState([])
   const [R, setR] = useState([])
-  const [sth, setSth] = useState(0)
+  const [noStates, setNoStates] = useState(3)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    let set = [1,2,3]
-    let numR = [0,1,2,3]
+    if (noStates==='') return
+    let set = []
+    let numR = []
+
+    for (let i=0;i<=noStates;i++){
+      if (i!==0) set[i-1]=i
+      numR[i]=i
+    }
+    setR(numR)
 
     let tmpStates = generateRk(set)
+    setNames(tmpStates)
+
     let statesArray = new Array(tmpStates.length)
 
     for (let i=0;i<statesArray.length;i++) {
@@ -26,20 +37,18 @@ function App() {
         statesArray[i][j] = ''//`${tmpStates[i]}${numR[j]}`
       }
     }
-
-    setNames(tmpStates)
     setStates(statesArray)
-    setR(numR)
-  }, [])
+  }, [noStates])
 
-  const handleChange = (i, j, value) => {
+  const handleChange = async (i, j, value) => {
+    await setLoading(true)
     let tmp = [...states]
     tmp[i][j] = value
     for (let i1=0;i1<names.length;i1++){
       for (let j1=j+1;j1<R.length;j1++){
-        let var1 = 'o'
-        let var2 = 'o'
-        let var3 = 'o'
+        let var1 = null
+        let var2 = null
+        let var3 = null
 
         for (let k=0;k<names.length;k++){
           /*console.log('Warunek 1: ',names[k].substring(0, 1), names[i1].substring(0, 1), names[k].substring(1, 2), j1, tmp[k][j1-1],
@@ -51,12 +60,20 @@ function App() {
         }
 
         tmp[i1][j1] = `${tmp[i1][j1-1]}+${var1}(${var2})*${var3}`
+        /*if (tmp[i1][j1-1]!=='') tmp[i1][j1] += `${tmp[i1][j1 - 1]}`
+        if (tmp[i1][j1-1]!=='' && var1!==null) tmp[i1][j1] += `+${var1}`
+        else if (tmp[i1][j1-1]==='' && var1!==null) tmp[i1][j1] += `${var1}`
+        if (var2!==null && var2!=='') tmp[i1][j1] += `(${var2})*`
+        if (var3!==null) tmp[i1][j1] += `${var3}`*/
       }
     }
-    setStates(tmp)
+    await setStates(tmp)
+    await setLoading(false)
   }
 
   return (
+      <>
+      <Navbar noStates={noStates} setNoStates={setNoStates} />
     <div className="container is-fluid">
       <div className='table-container'>
         <div className='table is-bordered is-striped is-narrow is-fullwidth'>
@@ -80,7 +97,7 @@ function App() {
             let counter=0;
             return <td key={`${i}${counter}`}>{names.map(name => {
               let num = counter++
-              return <tr key={`${i}${num}`} style={{display: 'flex'}}>R{i}/{name}: <input class='input is-small' type='text' value={states[num][i]} onChange={(e) => handleChange(num, i, e.target.value)} /></tr>
+              return <tr key={`${i}${num}`} style={{display: 'flex'}}>R{i}/{name}: <input className={`input is-small`} type='text' value={states[num][i]} onChange={(e) => handleChange(num, i, e.target.value)} /></tr>
             })}
             </td>
             })}
@@ -88,6 +105,7 @@ function App() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
--- src/functions/generateRk.js
+++ src/functions/generateRk.js
@@ -1,6 +1,4 @@
-let tab = []
-
-function printAllKLengthRec(set, prefix, n, k) {
+function printAllKLengthRec(tab, set, prefix, n, k) {
     if (k === 0) {
         tab.push(prefix)
         return
@@ -8,13 +6,14 @@ function printAllKLengthRec(set, prefix, n, k) {
     for (let i = 0; i < n; i++) {
         let newPrefix
         newPrefix = prefix + set[i]
-        printAllKLengthRec(set, newPrefix, n, k - 1)
+        printAllKLengthRec(tab, set, newPrefix, n, k - 1)
     }
 
 }
 
 function printAllKLength(set) {
-    printAllKLengthRec(set, "", set.length, 2)
+    let tab = []
+    printAllKLengthRec(tab, set, "", set.length, 2)
     return tab
 }
 
